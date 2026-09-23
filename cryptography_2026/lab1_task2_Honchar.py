cb6d019c37aa44f71c5140d4f244083d15141665
Anastasiia Honchar <anastasiagoncar@MacBook-Air---Anastasiia.local>
2026-09-23 11:48:46 +0300

    Lab 1: added functions to calculate entropy

--- cryptography_2026/lab1_task2_Honchar.py
+++ cryptography_2026/lab1_task2_Honchar.py
@@ -13,6 +13,14 @@ content_without = "abcdefabcdef"#f_without_spaces.read()
 
 count_with = Counter(content_with)
 count_without = Counter(content_without)
+length__with = len(content_with)
+length__without = len(content_without)
+
+
+global total_bigrams_1
+global total_bigrams_2
+global counted_bigram_words
+global counted_bigrams_without_spaces
 
 decorator = "\n==========================================================================\n"
 
@@ -25,7 +33,7 @@ def SeparateSymbolFrequency():
     print("'‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾'")
 
     for char, count in count_with.items():
-        frequency_ws = count / len(content_with)
+        frequency_ws = count / length__with
         print(f"'{char}' = {count} ->  {frequency_ws:.4f}")
     
 
@@ -34,7 +42,7 @@ def SeparateSymbolFrequency():
     print("| 1.ЧАСТОТА ОКРЕМИХ СИМВОЛІВ (текст без пробілів) |")
     print("'‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾'")
     for char, count in count_without.items():
-            frequency_wo = count / len(content_without)
+            frequency_wo = count / length__without
             print(f"'{char}' = {count} ->  {frequency_wo:.4f}")
 
 
@@ -80,5 +88,53 @@ def BigramsFrequency():
         frequency_bigrams_wo = count / total_bigrams_2
         print(f"'{bigram}' = {count} -> {frequency_bigrams_wo:.4f}")
 
+
+
+
+def EntropySeparateSymbols():
+
+    print("\n\n3.ЕНТРОПІЯ H1 для тексту з пробілами")
+    entropy_h1_ws = 0
+    for count in count_with.values():
+        p = count / length__with
+        entropy_h1_ws += -p * math.log2(p)
+    print("H1, окремі символи з пробілами:", entropy_h1_ws)
+
+    
+
+    print("\n3.ЕНТРОПІЯ H1 для тексту без пробілів")
+    entropy_h1_wo = 0
+    for count in count_without.values():
+        p = count / length__without
+        entropy_h1_wo += -p * math.log2(p)
+    print("H1, окремі символи без пробілів:", entropy_h1_wo)
+
+
+
+def EntropyBigrams():
+
+    print("\n\n4.ЕНТРОПІЯ H2 для тексту з пробілами")
+    entropy_h2_ws = 0
+    # for count in counted_bigram_words.values():
+    #     p = count / total_bigrams_1
+    #     entropy_h2_ws += -p * math.log2(p)
+    # print("H2, біграми з пробілами:", entropy_h2_ws)
+
+    
+
+    print("\n4.ЕНТРОПІЯ H2 для тексту без пробілів")
+    # entropy_h2_wo = 0
+    # for count in counted_bigrams_without_spaces.values():
+    #     p = count / total_bigrams_2
+    #     entropy_h2_wo += -p * math.log2(p)
+
+    #print("H2, біграми без пробілів:", entropy_h2_wo)
+
+
+
+
+
 #SeparateSymbolFrequency()
 BigramsFrequency()
+EntropySeparateSymbols()
+EntropyBigrams()
