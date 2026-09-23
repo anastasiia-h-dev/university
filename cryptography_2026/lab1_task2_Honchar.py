d82140fa18e2c9a261d896905c06756157a34143
Anastasiia Honchar <anastasiagoncar@MacBook-Air---Anastasiia.local>
2026-09-23 11:25:11 +0300

    Lab 1: updated script to calculate the frequencies of bigrams in both texts

--- cryptography_2026/lab1_task2_Honchar.py
+++ cryptography_2026/lab1_task2_Honchar.py
@@ -7,8 +7,8 @@ import math
 
 f_with_spaces = open("text_w_spaces.txt", "r")
 f_without_spaces = open("text_wo_spaces.txt", "r")
-content_with = f_with_spaces.read()
-content_without = f_without_spaces.read()
+content_with = "abcdefabcdef"#f_with_spaces.read()
+content_without = "abcdefabcdef"#f_without_spaces.read()
 
 
 count_with = Counter(content_with)
@@ -37,3 +37,48 @@ def SeparateSymbolFrequency():
             frequency_wo = count / len(content_without)
             print(f"'{char}' = {count} ->  {frequency_wo:.4f}")
 
+
+
+def BigramsFrequency():
+    #для біграм
+    print(decorator)
+    print("._________________________________________________________________________.")
+    print("| 2.ЧАСТОТА БІГРАМ (для тексту з пробілами) : біграми - слова, з перетином|")
+    print("'‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾'")
+
+    bigram_words = re.findall("\w+", content_with)
+    counted_bigram_words = Counter(zip(bigram_words, islice(bigram_words, 1, None)))
+    total_bigrams_1 = sum(counted_bigram_words.values())
+    for pair, count in counted_bigram_words.items():
+        bigram_words_freq = count / total_bigrams_1
+        print(f"{pair} = {count} -> {bigram_words_freq:.4f}")
+
+    
+
+    print(decorator)
+    print(".____________________________________________.")
+    print("| 2.ЧАСТОТА БІГРАМ (для тексту без пробілів) : біграми - пари сивмолів, без перетину|")
+    print("'‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾'")
+    bigrams_without_spaces = [content_without[i : i + 2] for i in range(0, len(content_without) - 1, 2)]
+    counted_bigrams_without_spaces = Counter(bigrams_without_spaces)
+    total_bigrams_2 = sum(counted_bigrams_without_spaces.values())
+    
+    for bigram, count in counted_bigrams_without_spaces.items():
+        frequency_bigrams_wo = count / total_bigrams_2
+        print(f"'{bigram}' = {count} -> {frequency_bigrams_wo:.4f}")
+
+    print(decorator)
+    print(".____________________________________________.")
+    print("| 2.ЧАСТОТА БІГРАМ (для тексту без пробілів) : біграми - пари сивмолів, з перетином|")
+    print("'‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾'")
+
+    bigrams_without_spaces = [content_without[i : i + 2] for i in range(0, len(content_without) - 1, 1)]
+    counted_bigrams_without_spaces = Counter(bigrams_without_spaces)
+    total_bigrams_2 = sum(counted_bigrams_without_spaces.values())
+    
+    for bigram, count in counted_bigrams_without_spaces.items():
+        frequency_bigrams_wo = count / total_bigrams_2
+        print(f"'{bigram}' = {count} -> {frequency_bigrams_wo:.4f}")
+
+#SeparateSymbolFrequency()
+BigramsFrequency()
